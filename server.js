5f8c3cc551e5d58d0c86bb155c2f0d619c856895
Umesh-Mathulkar <mathulkar98@gmail.com>
2024-03-08 16:37:43 +0530

    room correction

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const socketio = require('socket.io');
 const app = require('./app');
 const { ChatRoom, Message } = require("./models/ChatRoom");
-require('dotenv').config(); 
+require('dotenv').config();
 
 const server = http.createServer(app);
 const io = require('socket.io')(server, {
@@ -14,24 +14,33 @@ const io = require('socket.io')(server, {
 io.on('connection', (socket) => {
   console.log('New WebSocket connection');
 
-  socket.on('chatMessage', async (msg, participants, senderEmail) => {
-    
+  socket.on('joinRoom', (roomId) => {
+    socket.join(roomId);
+    console.log(`User joined room ${roomId}`);
+  });
+
+  socket.on('leaveRoom', (roomId) => {
+    socket.leave(roomId);
+    console.log(`User left room ${roomId}`);
+  });
+
+  socket.on('chatMessage', async (msg, participants, senderEmail, roomId) => {
     try {
       let chatroom = await ChatRoom.findOne({ participants: { $all: participants } });
       if (!chatroom) {
         chatroom = new ChatRoom({ participants: participants });
       }
-  
+
       const newMessage = new Message({
         sender: senderEmail,
         message: msg,
       });
-  
+
       chatroom.messages.push(newMessage);
       await chatroom.save();
-  
-     // Emit the new message as an object
-    io.emit('message', { sender: senderEmail, message: msg, timestamp: new Date() });
+
+      // Emit the new message as an object to the specific room
+      io.to(roomId).emit('message', { sender: senderEmail, message: msg, timestamp: new Date() });
     } catch (error) {
       console.error('An error occurred while posting the message:', error);
     }
